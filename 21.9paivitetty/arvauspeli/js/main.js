580711e1408096948d5ddec1427176993af7d745
uuberr <60771293+uuberr@users.noreply.github.com>
2020-09-22 11:29:58 +0300

    Add files via upload
    korjattu arvauspeli

--- 21.9paivitetty/arvauspeli/js/main.js
+++ 21.9paivitetty/arvauspeli/js/main.js
@@ -1,13 +1,8 @@
-    var i = 0;
-    var numero = 0;
-    function arpoa() {
-        document.getElementById('arvottu').innerHTML = "Numero on arvottu";
-        numero = Math.floor(Math.random() * 10);
-        console.log(numero);    
-    }
-    function peli() {
-        document.getElementById('arvottu').innerHTML = "";
-        const syot = parseInt(document.getElementById('inpt').value);
+var i = 0;
+var numero = Math.floor(Math.random() * 10) + 1;
+function peli() {
+    document.getElementById('arvottu').innerHTML = "";
+    const syot = parseInt(document.getElementById('inpt').value);
         if (syot < numero) {
             document.getElementById('numon').innerHTML =  "Numero on: suurempi";
             document.getElementById('gz').innerHTML =  "";
@@ -21,8 +16,9 @@
             document.getElementById('gz').innerHTML =  "Onneksi olkoon voitit";
             i++;
             document.getElementById('pist').innerHTML = i;
+            document.getElementById('arvottu').innerHTML = "Uusi numero on arvottu";
+            numero = Math.floor(Math.random() * 10) + 1;
         }
         else {
-            
         }
     }
